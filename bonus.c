6bc147ac16606295f4ff70502b964c50867219b7
comdiv <fagim.sadykov@gmail.com>
2022-12-05 22:57:07 +0300

    fix bonus.c

--- bonus.c
+++ bonus.c
@@ -15,8 +15,13 @@ void init_Bonus(int ticketCount, Bonus *target)
     *target = tmp;
 }
 
+// private defs
+
 int __calculate_per_men(Bonus* target);
 int __calculate_per_female(Bonus* target);
+void __calculate_by_baseStrategy(Bonus *target);
+
+// public impls
 
 void calculate_Bonus(Bonus* target) {
 
@@ -27,7 +32,10 @@ void calculate_Bonus(Bonus* target) {
     /* many calls after ...*/
 }
 
-int __calculate_by_baseStrategy(Bonus *target) {
+
+// private impls
+
+void __calculate_by_baseStrategy(Bonus *target) {
     // PROBLEM (3) WE DON'T WANT TO STEP BY STEP WORK WITH ERRORS ON ALL CALLS (WE ARE LAZY)
     target->ticketPerMen = __calculate_per_men(target);
     target->ticketPerFemale = __calculate_per_female(target);
